1be4655d4bc0eaa2195364298ee79de461e9d672
Random <ehabflash833@gmail.com>
2022-09-08 02:12:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ if __name__=='__main__':
         command = CommandFactory.create_commands(args)
         command.execute()
     except Exception as e:
-        output_object = OutPut(status = e.status, message = e.message)
+        output_message = OutPut(status = e.status, message = e.message)
     else:
-        output_object = OutPut(status = Status.SUCCESS)
+        output_message = OutPut(status = Status.SUCCESS)
 
-print(output_object.message)
+print(output_message.message)
