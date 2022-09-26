ea72344a783b32481badefebc69c209cb30bf314
josephShokry <josephshokry9@gmail.com>
2022-09-26 10:45:31 +0200

    solving the conflict with the index.py

--- source/models/index.py
+++ source/models/index.py
@@ -1,5 +1,4 @@
 import os
-# from models.table import Table
 from outputs.exceptions import *
 
 class Index:
@@ -10,40 +9,18 @@ class Index:
 
     def serialize(self):
         os.makedirs(self.get_path(), exist_ok = True)
-    
-    # def get_path(self):
-    #     return os.path.join(self.table.get_path(), os.path.join("indices" , self.index_name))
-    
-    # def add_primary_key(self, primary_key):
-    #     index_value_file_path = os.path.join(self.get_path(), self.index_value + ".txt")
-    #     old_primary_keys = set()
-    #     if os.path.isfile(index_value_file_path):
-    #         old_primary_keys = set(self.get_primary_key(self.index_value))                
-    #     old_primary_keys.add(primary_key)
-    #     Index.__write_in_file(path = index_value_file_path, data = old_primary_keys)
-    
-    # def delete_primary_key(self, primary_key, index_value):
-    #     index_value_file_path = os.path.join(self.get_path(), index_value + ".txt")
-    #     if not os.path.isfile(index_value_file_path):
-    #         raise FileNotFound(message = "there is no file with this index : " + index_value)
-    #     old_primary_keys = self.get_primary_key(index_value)
-    #     old_primary_keys.remove(primary_key)
-    #     Index.__write_in_file(path = index_value_file_path, data = old_primary_keys)
 
-    # def get_primary_key(self, index_value):
-    #     index_value_file_path = os.path.join(self.get_path(), index_value + ".txt")
-    #     with open(index_value_file_path, mode ="r") as index_file:
     def get_path(self, index_value = None):
         if index_value == None:
-            return os.path.join(self.table.get_path(), os.path.join("indices" , self.index_value))
+            return os.path.join(self.table.get_path(), os.path.join("indices" , self.index_name))
         else:
             return os.path.join(self.get_path(), self.index_value + ".txt")
 
-    def add_primary_key(self, primary_key, index_value):
-        old_primary_keys = set(self.get_primary_key(index_value))                
+    def add_primary_key(self, primary_key):
+        old_primary_keys = set(self.get_primary_key(self.index_value))                
         old_primary_keys.add(primary_key)
-        Index.__write_in_file(path = self.get_path(index_value), data = old_primary_keys)
-    
+        self.__write_in_file(data = old_primary_keys)
+
     def delete_primary_key(self, primary_key, index_value):
         old_primary_keys = self.get_primary_key(index_value)
         old_primary_keys.remove(primary_key)
@@ -61,5 +38,5 @@ class Index:
         self.add_primary_key(primary_key = primary_key, index_value = index_value)
 
     def __write_in_file(self, data):
-        with open(self.get_index_value_path(self.index_value), mode ="w") as index_file:
+        with open(self.get_path(self.index_value), mode ="w") as index_file:
             index_file.write(",".join(data))
