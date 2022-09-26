f504838b45ec61247fdbff7a9681772c63aad2dd
josephShokry <josephshokry9@gmail.com>
2022-09-26 22:33:50 +0200

    adding the lock

--- source/models/row.py
+++ source/models/row.py
@@ -1,5 +1,4 @@
 import os, json, uuid
-from commands_functions.schema_keys import Keys
 from outputs.exceptions import *
 from models.index import Index
 
@@ -7,35 +6,26 @@ class Row:
     def __init__(self, table, value = None):
         self.table = table
         self.value = value
+        if self.table.table_metadata.primary_key not in value : 
+            value[self.table.table_metadata.primary_key] = str(uuid.uuid4().node)
         self.primary_key = value[self.table.table_metadata.primary_key]
 
-        # if value is not None:
-        #     self.__init_by_value(value)
-        # elif primary_key is not None:
-        #     self.__init_by_primary_key()
-        # else:
-        #     raise WrongInput(message = "the value and PK of the row is null")
-        
-    # def __init_by_value(self, value):
-
-    # def __init_by_primary_key(self,primary_key):
-        # self.primary_key = primary_key
-        # self.__get_by_primary_key()
-
     def serialize(self):
+        self.__lock()
         self.__colomns_name_validate()
         if self.table.table_metadata.primary_key not in self.value : 
             self.value[self.table.table_metadata.primary_key] = str(uuid.uuid4().node)
         row_json_data = json.dumps(self.value, indent = 2)
-        with open(self.get_path(), 'x') as row_file: 
+        with open(self.get_path(), 'w') as row_file: 
             row_file.write(row_json_data)
         self.update_index()
+        self.__unlock()
 
     def row_exists(self):
         return os.path.isfile(self.get_path())
 
     def get_path(self):
-        return os.path.join(self.table.get_path(), self.primary_key + ".json")
+        return os.path.join(self.table.get_path(), os.path.join("Data", self.primary_key + ".json"))
 
     def __colomns_name_validate(self):
         for row_colomn_name in self.value:
@@ -45,10 +35,10 @@ class Row:
     def update_index(self):
         for index_name in self.table.table_metadata.index_keys:
             index = Index(self.table, index_name = index_name, index_value = self.value[index_name])
-            index.update_primary_key(primary_key = self.primary_key) ##update
+            index.update_primary_key(primary_key = self.primary_key) 
     
     @staticmethod
-    def __load_by_primary_key(table, primary_key):
+    def load_by_primary_key(table, primary_key):
         row_file_path = os.path.join(table.get_path(), primary_key + ".json")
         if not os.path.isfile(row_file_path) :
             return None
@@ -57,8 +47,23 @@ class Row:
         return Row(value = value)
 
     def delete(self):
-        if self.row_exists():
-            os.remove(self.get_path())
-            for index_name in self.table.table_metadata.index_keys:
-                index = Index(self, index_name = index_name, index_value = self.value[index_name])
-                index.delete_primary_key(primary_key = self.primary_key, index_value = self.value[index_name])
+        self.__lock()
+        if not self.row_exists():
+            return
+        os.remove(self.get_path())
+        for index_name in self.table.table_metadata.index_keys:
+            index = Index(self, index_name = index_name, index_value = self.value[index_name])
+            index.delete_primary_key(primary_key = self.primary_key, index_value = self.value[index_name])
+        self.__unlock()
+
+    def __lock(self):
+        lock_file_path = os.path.join(self.table.get_path(), os.path.join("Lock", self.primary_key + ".json"))
+        try:
+            with open(lock_file_path, "x"):
+                pass
+        except:
+            raise FileNotFound(message = "the primary key is locked now try again later")
+
+    def __unlock(self):
+        lock_file_path = os.path.join(self.table.get_path(), os.path.join("Lock",self.primary_key + ".json"))
+        os.remove(lock_file_path)
