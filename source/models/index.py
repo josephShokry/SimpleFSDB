8a12772712db519d2f362797bf855557a545defc
josephShokry <josephshokry9@gmail.com>
2022-09-28 09:20:34 +0200

    fix comment
    - changing the write_in_file function to update_in_file funtion

--- source/models/index.py
+++ source/models/index.py
@@ -18,14 +18,15 @@ class Index:
     def add_primary_key(self, primary_key, index_value):
         old_primary_keys = set(self.get_primary_key(index_value))                
         old_primary_keys.add(primary_key)
-        self.__write_in_file(data = old_primary_keys, index_value = index_value)
+        self.__update_in_file(data = old_primary_keys, index_value = index_value)
 
     def delete_primary_key(self, primary_key, index_value):
         old_primary_keys = self.get_primary_key(index_value)
         if primary_key in old_primary_keys : old_primary_keys.remove(primary_key) 
-        if len(old_primary_keys)>0: self.__write_in_file(data = old_primary_keys,index_value = index_value)
-        elif os.path.isfile(self.get_path(index_value)) :
-            os.remove(self.get_path(index_value))
+        self.__update_in_file(data = old_primary_keys, index_value = index_value)
+        # if len(old_primary_keys)>0: self.__write_in_file(data = old_primary_keys,index_value = index_value)
+        # elif os.path.isfile(self.get_path(index_value)) :
+        #     os.remove(self.get_path(index_value))
 
     def get_primary_key(self, index_value):
         if not os.path.isfile(self.get_path(index_value)):
@@ -38,6 +39,15 @@ class Index:
         self.delete_primary_key(primary_key = primary_key, index_value = index_value)
         self.add_primary_key(primary_key = primary_key, index_value = index_value)
 
-    def __write_in_file(self, data, index_value):
-        with open(self.get_path(index_value), mode ="w") as index_file:
-            index_file.write(",".join(data))
+    def __update_in_file(self, data, index_value):
+        if len(data) == 0 and os.path.isfile(self.get_path(index_value)) :
+            os.remove(self.get_path(index_value))
+        elif len(data)>0:
+            with open(self.get_path(index_value), mode ="w") as index_file:
+                index_file.write(",".join(data))
+
+        # if len(data)>0: 
+        #     with open(self.get_path(index_value), mode ="w") as index_file:
+        #         index_file.write(",".join(data))
+        # elif os.path.isfile(self.get_path(index_value)) :
+        #     os.remove(self.get_path(index_value))
